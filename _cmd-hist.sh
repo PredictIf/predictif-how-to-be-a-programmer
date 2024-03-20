8070150d397dca940fc36f664b8801d4b0501255
Marian Dumitrascu <106326037+mariandumitrascu-p@users.noreply.github.com>
2024-03-20 14:11:47 -0500

    ssdfdsf

--- _cmd-hist.sh
+++ _cmd-hist.sh
@@ -8,3 +8,6 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/PredictIf/predictif-how-to-be-a-programmer.git
 git push -u origin main
+
+
+Create an app that determines the perfect life partner for anyone in the world, based solely on their digital footprint.
